225b625bab09a206ebfbd707ac8f92740b1eeaf0
Taylan Durmaz <merttaylandurmaz@gmail.com>
2020-05-31 23:17:30 +0300

    help message

--- src/Components/Layout/Header/index.jsx
+++ src/Components/Layout/Header/index.jsx
@@ -1,16 +1,34 @@
-import React from "react";
-import { Row } from "antd";
+import React, { useState } from "react";
+import { Row, Modal } from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import { useTheme } from "styled-components";
 import Text from "../../Text";
 import HeaderStyles from "./styles";
-import BAU_LOGO from "../../../Assets/bau.jpg";
 
 function Header() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const theme = useTheme();
+
   return (
     <HeaderStyles>
       <Row justify="center" align="middle">
-        <img className="logo" src={BAU_LOGO} alt="BAU" />
+        <QuestionCircleOutlined
+          style={{ fontSize: 50, marginRight: 5, color: theme.main.primary }}
+          onClick={() => setModalVisible(true)}
+        />
+
         <Text style={{ fontWeight: "bold" }}>CMP3004 Project Visualizer</Text>
       </Row>
+
+      <Modal
+        visible={modalVisible}
+        onCancel={() => setModalVisible(false)}
+        footer={false}
+      >
+        It is a tool for those who do not want to waste time with visualizing.
+        <br />
+        <Text bold>Just change the tour with your own.</Text>
+      </Modal>
     </HeaderStyles>
   );
 }
